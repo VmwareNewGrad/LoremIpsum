49b1d8f52026411306525fed0bcff316ddfe6a4f
chengwang86 <chengwang86@users.noreply.github.com>
2016-11-03 22:59:27 -0700

    Create generate.js

---
+++ static/js/generate.js
@@ -0,0 +1,46 @@
+function getLorem() {
+
+var area = document.getElementById("textOutput");
+var n = document.getElementById("123").value;
+
+if(document.getElementById('paraPick').checked) {
+
+   $.ajax({
+            url: '/getP?num='+n,
+            success: function(response) {
+                area.innerHTML = response;
+            },
+            error: function(error) {
+                alert(error);
+            }
+        });
+
+} else if(document.getElementById('wordPick').checked) {
+
+    $.ajax({
+            url: '/getW?num='+n,
+            success: function(response) {
+                area.innerHTML = response;
+            },
+            error: function(error) {
+                alert(error);
+            }
+        });
+
+
+} else if(document.getElementById('letterPick').checked) {
+
+    $.ajax({
+            url: '/getL?num='+n,
+            success: function(response) {
+                area.innerHTML = response;
+            },
+            error: function(error) {
+                alert(error);
+            }
+        });
+
+
+} else {
+alert("error: No radio button checked");
+}
